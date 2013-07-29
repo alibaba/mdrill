b7d111f520d456a6acfc2127e5b30c174f2e9026
muyannian <myn@163.com>
2013-07-29 18:15:30 +0800

    实时部分MlogReader

--- trunk/adhoc-solr/src/main/java/com/alimama/mdrill/solr/realtime/mlog/MLogSigmentReader.java
+++ trunk/adhoc-solr/src/main/java/com/alimama/mdrill/solr/realtime/mlog/MLogSigmentReader.java
@@ -3,59 +3,54 @@ package com.alimama.mdrill.solr.realtime.mlog;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 import com.alimama.mdrill.solr.realtime.IMessage;
 
-public class MLogSigmentWrite {
+public class MLogSigmentReader {
 	private Configuration conf;
 	private FileSystem fs;
-
-	private FSDataOutputStream write;
-
 	private final long startoffset;
-
-	public MLogSigmentWrite(Configuration conf, String filepath, final long offset)
+	FSDataInputStream reader;
+	public MLogSigmentReader(Configuration conf, String filepath, final long offset)
 			throws IOException {
 		this.conf = conf;
 		this.fs = FileSystem.get(this.conf);
-		this.write = this.fs.create(new Path(filepath));
+		this.reader = this.fs.open(new Path(filepath));
 		this.startoffset = offset;
 	}
-
-
-	public void append(final IMessage msg) throws IOException {
-		synchronized (this) {
-			byte[] d=msg.getData();
-			this.write.writeInt(d.length);
-			this.write.write(d);
-		}
-
+	
+	public void read(final IMessage msg) throws IOException
+	{
+		byte[] d=this.readByte();
+		msg.setData(d);
 	}
-
+	
+	public byte[] readByte() throws IOException
+	{
+		synchronized (this) {
+			int size=reader.readInt();
+			byte[] d=new byte[size];
+			reader.read(d);
+			return d;
+		}
+	}
+	
 	public long getPos() throws IOException {
-		return this.startoffset + this.write.getPos();
+		return this.startoffset + this.reader.getPos();
 	}
 
 	public long getCurrPos() throws IOException {
-		return this.startoffset + this.write.getPos();
+		return this.startoffset +this.reader.getPos();
 	}
-
-	public void sync() throws IOException {
-		synchronized (this) {
-			this.write.flush();
-			this.write.sync();
-		}
-
-	}
-
+	
+	
 	public void close() throws IOException {
 		synchronized (this) {
-			this.write.close();
+			this.reader.close();
 		}
 
 	}
-
 }
